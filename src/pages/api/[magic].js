5d0e8bde9761a1b1528cae957da4344c09307943
Juan Pablo Solana Ortiz <pablo@Juans-MacBook-Pro.local>
2023-02-03 14:59:03 -0600

    🔥 new categories file

--- src/pages/api/[magic].js
+++ src/pages/api/[magic].js
@@ -6,12 +6,15 @@ cohere.init(process.env.COHERE_API_KEY); // This is your trial API key
 export default function handler(req, res) {
   const { magic } = req.query
   const magicFormat = magic.replace(/-/g, ' ')
-  async function makeTweet () {
-    const response = await cohere.generate(cohereData(magicFormat));
-    const { statusCode } = response;
-    const { body } = response;
-    if (statusCode !== 200) res.status(500).send('Timeout')
-    res.send(body.generations[0].text);
+  async function makeTweet() {
+    try {
+      const response = await cohere.generate(cohereData(magicFormat));
+      const { body } = response;
+      res.send(body.generations[0].text);
+    }
+    catch (error) {
+      res.status(500).send('Timeout', error);
+    }
   }
   makeTweet(magicFormat)
 }

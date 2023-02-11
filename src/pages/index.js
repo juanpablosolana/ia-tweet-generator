b4a5687348ad54cdf111886a786a2ce2edc33fa0
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2023-02-11 13:02:45 -0600

    📱fix mobile view

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,16 +49,9 @@ export default function Home() {
       </Head>
       <header>
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-              <Image src={imgGenerator} className="mr-3 h-6 sm:h-9" alt="Tweet generator Logo" width={50} height={50} />
-              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Tweet Generator</span>
-            <div className="flex items-center lg:order-2">
-              <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
-                <span className="sr-only">Open main menu</span>
-                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-              </button>
-            </div>
+          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl sm:flex">
+            <Image src={imgGenerator} className="mr-3 h-6 sm:h-9 rounded-full" alt="Tweet generator Logo" width={50} height={50} />
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Tweet Generator | BETA</span>
           </div>
         </nav>
       </header>
@@ -72,9 +65,9 @@ export default function Home() {
               : <DataList handlerMagic={handlerMagic} CATEGORIES={CATEGORIES} />
             }
           </div>
-          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex items-center flex-col gap-4">
+          <div className="py-2 lg:mt-0 lg:col-span-5 lg:flex items-center flex-col gap-4">
             {
-              isLoding ? <Loader /> : <Image src={imgGenerator} width={400} height={400} alt='Image create by AI' />
+              isLoding ? <Loader /> : <Image src={imgGenerator} width={400} height={400} alt='Image create by AI' className='rounded-lg'/>
             }
             {isLoding? <Loader/>:<p>{message}</p>}
             <button onClick={handlerTweet} classNameName= "inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Share tweet!</button>
@@ -84,7 +77,7 @@ export default function Home() {
       <footer className="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
         <div className="mx-auto max-w-screen-xl text-center">
           <footer className="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
-            <Image src={imgGenerator} className="mr-3 h-6 sm:h-9" alt="Tweet generator Logo" width={50} height={50} />
+            <Image src={imgGenerator} className="mr-3 h-6 sm:h-9 rounded-full" alt="Tweet generator Logo" width={50} height={50} />
             Tweet Generator
           </footer>
           <p className="my-6 text-gray-500 dark:text-gray-400">Amazing AI tweet generation</p>

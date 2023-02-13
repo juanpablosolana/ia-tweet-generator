a5f15b33d16aefe5a0d41082bbb8312c0ec305ea
Juan Pablo Solana Ortiz <pablo@Juans-MacBook-Pro.local>
2023-02-13 15:31:01 -0600

    🍨 install google tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
+import { GTMscript } from '@/components/Gtag';
 
 
 export default function App({ Component, pageProps }) {
   return <>
     <Component {...pageProps} />
     <Analytics />
+    <GTMscript />
   </>
 }

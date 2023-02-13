a0d091f262962e3b15d5a5f4573913aeecde38e4
Juan Pablo Solana Ortiz <pablo@Juans-MacBook-Pro.local>
2023-02-13 14:20:12 -0600

    fix text desing

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -25,7 +25,7 @@ export default function MainSection({ handlerMagic, isLoding, imgGenerator, mess
           {
             isLoding
               ? < TextSkeletor />
-              : <p className="text-xl font-semibold text-gray-900 dark:text-white">{message}</p>
+              : <p className="font-semibold text-gray-500 dark:text-gray-400">{message}</p>
           }
           {
             !isLoding && <button onClick={handlerTweet} className="inline-flex justify-center items-center py-3 px-5 my-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Share on Twitter!</button>

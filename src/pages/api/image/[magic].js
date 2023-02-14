bfdd532db20999a424bc728bcb1c5d484415d0c4
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2023-02-14 16:51:55 -0600

    resolve conflict

--- src/pages/api/image/[magic].js
+++ src/pages/api/image/[magic].js
@@ -4,11 +4,7 @@ import { createImage } from '@/services/ia'
 const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY })
 const openai = new OpenAIApi(configuration)
 
-<<<<<<< Updated upstream
-export default async function handler(req, res) {
-=======
 export default async function handler (req, res) {
->>>>>>> Stashed changes
   if (req.method !== 'GET') return res.status(405).send({})
 
   const { magic } = req.query

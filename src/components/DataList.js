66f66f499b48fa3d1745c70e1008f34cbe72f6b3
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2023-03-02 13:51:26 -0600

    news tweet label wordings

--- src/components/DataList.js
+++ src/components/DataList.js
@@ -2,8 +2,8 @@ export default function DataList ({ handlerMagic }) {
   return (
     <form className='space-y-4 md:space-y-6' onSubmit={handlerMagic}>
       <div>
-        <label htmlFor='topics' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Write a topic</label>
-        <input name='topics' id='topics' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='pets' required='' />
+        <label htmlFor='topics' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Type a subject for which you would like to generate a tweet:</label>
+        <input name='topics' id='topics' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='e.g: pets, Beethoven history, mexican food' required='' />
       </div>
       <button type='submit' className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>Generate!</button>
     </form>
--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -10,10 +10,16 @@ export default function MainSection ({ handlerMagic, isLoding, imgGenerator, mes
       <div className='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
         <div className='mr-auto place-self-center lg:col-span-7'>
           <h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white'>Tweet Generator</h1>
-          <p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400'>Tweet Generator is a project to generate artificially intelligent tweets. <br />The project uses natural language processing and deep learning algorithms to generate tweets that are similar to what humans would write. <br /> The project is currently in early stages of development, but it is a promising start for automated tweet generation.</p>
-          {isLoding
-            ? <Loader />
-            : <DataList handlerMagic={handlerMagic} />}
+          <p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400'>
+            Tweet Generator is a project to generate artificially intelligent tweets. <br />
+            Uses natural language processing and deep learning algorithms to generate tweets that are similar to what humans would write. <br />
+            Is currently in early stages of development, but it is a promising start for automated tweet generation.
+          </p>
+          {
+            isLoding
+              ? <Loader />
+              : <DataList handlerMagic={handlerMagic} />
+          }
         </div>
         <div className='py-2 lg:mt-0 lg:col-span-5 lg:flex items-center flex-col gap-4'>
           {

98cfe07e561f50b7dafa9da86b36dec501287745
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2023-03-02 13:53:44 -0600

    🚑 Hotfix fix footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer () {
   return (
     <footer className='bg-white md:p-8 lg:p-4 dark:bg-gray-800'>
       <div className='mx-auto max-w-screen-xl text-center'>
-        <p className='my-2 text-gray-500 dark:text-gray-400'>Amazing AI tweet generatior | BETA</p>
+        <p className='my-2 text-gray-500 dark:text-gray-400'>Amazing AI Tweet Generator | BETA</p>
         <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
           Developed by <Link href='https://pablosolana.dev' className='hover:underline'>Pablo Solana {' | '}</Link>
           <Link href='/privacity' className='hover:underline'>

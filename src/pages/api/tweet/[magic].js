e27492dfe3b1446e479e9cbacd1ccf0f84cc32df
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2023-08-03 17:17:59 -0600

    cors test

--- src/pages/api/tweet/[magic].js
+++ src/pages/api/tweet/[magic].js
@@ -5,6 +5,15 @@ const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY })
 const openai = new OpenAIApi(configuration)
 
 export default async function handler (req, res) {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   if (req.method !== 'GET') return res.status(405).send({})
 
   const { magic } = req.query

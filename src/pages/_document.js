d7c8da862cf8f7faf482a26b8700e38c052194bd
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2023-08-03 15:14:47 -0600

    add google adsense configuration

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { GTMnoscript } from '@/services/gtag'
+import Script from 'next/script'
 
 export default function Document () {
   return (
     <Html lang='en'>
+      <Script
+        async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8152954513756798'
+        crossorigin='anonymous'
+      />
       <Head />
       <body>
         <GTMnoscript />
